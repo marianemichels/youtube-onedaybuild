d87efc81ac250f9568d3695e5cf8425d2b4bcd30
Mariane Michels <marianemichels@Mariane-iMac.local>
2020-10-20 10:38:48 -0600

    added code to parse the json response

---
+++ youtube-onedaybuild/Response.swift
@@ -0,0 +1,27 @@
+//
+//  Response.swift
+//  youtube-onedaybuild
+//
+//  Created by Mariane Michels on 2020-10-20.
+//
+
+import Foundation
+
+struct Response : Decodable {
+    
+    var items: [Video]?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case items
+    }
+    
+    init (from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+        
+        
+    }
+}
