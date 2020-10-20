6199260b225803cfee2a715de7313963377b7d3f
Mariane Michels <marianemichels@Mariane-iMac.local>
2020-10-20 09:27:49 -0600

    modified gitignore to stop tracking Constants.swift

---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,17 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Mariane Michels on 2020-10-19.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+}
