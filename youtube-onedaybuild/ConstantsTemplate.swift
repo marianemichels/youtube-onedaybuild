d2e7a3e391d8f763d0cce6c493e3dac8b1b402d0
Mariane Michels <marianemichels@Mariane-iMac.local>
2020-10-21 09:35:17 -0600

    Added ConstantsTemplate.swift file, added tableview, custom protocol in the model, displaying video titles in the tableview

---
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -0,0 +1,23 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-onedaybuild
+//
+//  Created by Mariane Michels on 2020-10-21.
+//
+
+import Foundation
+
+// Copy of the Constants.swift file that gets uploaded to the repository without sensitive information (API_KEY)
+
+
+
+struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL =
+        "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+    
+    static var VIDEOCELL_ID = "VideoCell"
+}
+ 
--- youtube-onedaybuild/Model.swift
+++ youtube-onedaybuild/Model.swift
@@ -7,7 +7,14 @@
 
 import Foundation
 
+protocol ModelDelegate {
+    
+    func videosFetched(_ videos:[Video])
+}
+
 class Model {
+    
+    var delegate:ModelDelegate?
     
     func getVideos() {
         
@@ -37,7 +44,17 @@ class Model {
                 
                 let response = try decoder.decode(Response.self, from: data!)
                 
-                dump(response)
+                if response.items != nil {
+                    
+                    DispatchQueue.main.async {
+                        // Call the videosFetched method of the delegate
+                        self.delegate?.videosFetched(response.items!)
+                    }
+                    
+                    
+                }
+                
+                // dump(response)
                 
             }
             catch {
