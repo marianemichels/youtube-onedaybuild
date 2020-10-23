1ff708acaed6d985a95868a5eab27a3e74719d5a
Mariane Michels <marianemichels@Mariane-iMac.local>
2020-10-23 09:59:47 -0600

    added detailviewcontroller, segue tot detail, and webview in detailviewcontroller

--- youtube-onedaybuild/Helpers/ConstantsTemplate.swift
+++ youtube-onedaybuild/Helpers/ConstantsTemplate.swift
@@ -19,6 +19,7 @@ struct Constants {
         "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
     static var VIDEOCELL_ID = "VideoCell"
+    static var YT_EMBED_URL = "https://www.youtube.com/embed/"
 }
  */
  
